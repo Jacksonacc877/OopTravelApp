7975276745351a793c9f3e085111790bf83fb697
b07505022 <b07505022@ntu.edu.tw>
2020-06-20 16:29:38 +0800

    \

--- app/src/main/java/com/example/oop_travel_app/order_function/Account.java
+++ app/src/main/java/com/example/oop_travel_app/order_function/Account.java
@@ -4,23 +4,64 @@ import android.content.Context;
 
 import com.example.oop_travel_app.database_function.DBOperation;
 
+import java.util.ArrayList;
+
 public class Account {
 
-	public boolean register(){
-
-		return false;
+	public String getID() {
+		return ID;
 	}
-	public boolean login(){
+	private String ID;
 
-		return false;
+	public String getUserName() {
+		return userName;
 	}
-	public String  outputData(){
+	private String userName;
 
-
-		return null;
+	public String getPassword() {
+		return password;
 	}
-	public String  revise(String col){
+	private String password;
 
+	public String getUserPhone() {
+		return userPhone;
+	}
+	private String userPhone;
+
+	FireStoreHelper fbs = new FireStoreHelper();
+	public boolean register(String ID, String userName, String password, String userPhone){
+		ArrayList<String> userIDs = fbs.getUserIDs();
+		for(String str:userIDs){
+			if(str.contentEquals(ID)){
+
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+	}
+	public boolean login(String ID, String password) {
+		ArrayList<String> data = fbs.getData();
+		for (String str : data) {
+			if (str.contentEquals(account)) {
+
+
+				if (str.contentEquals(password)) {
+
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+		}
+	}
+	public ArrayList<String> outputData(ArrayList<String> data){
+		return data;
+	}
+	public String revise(String userName, String password, String checkedpassword, String userPhone){
+		if(!userName.equals(null))
 
 		return null;
 	}
