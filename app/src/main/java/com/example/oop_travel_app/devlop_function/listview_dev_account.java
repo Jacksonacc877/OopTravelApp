146ddca409c959cef34f5619e8015ae30efeac34
jacksonacc877 <b07505035@ntu.edu.tw>
2020-06-22 02:53:09 +0800

    devlop is all done

--- app/src/main/java/com/example/oop_travel_app/devlop_function/listview_dev_account.java
+++ app/src/main/java/com/example/oop_travel_app/devlop_function/listview_dev_account.java
@@ -1,10 +1,13 @@
 package com.example.oop_travel_app.devlop_function;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+
+import com.example.oop_travel_app.R;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -21,26 +24,44 @@ public class listview_dev_account extends BaseAdapter {
     }
     @Override
     public int getCount() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return list.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        Object obj=list.get(position);
+        return list.indexOf(obj);
     }
     private static class ViewHolder{
-        TextView triptitle;
-        TextView priceinterval;
-        TextView dateinterval;
+        TextView setaccount;
+        TextView setusername;
+        TextView setphone;
+        TextView setpassword;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        listview_dev_account.ViewHolder holder;
+        if(convertView==null){
+            convertView= LayoutInflater.from(context).inflate(R.layout.listview_dev_account, parent,false);
+            holder =new listview_dev_account.ViewHolder();
+            holder.setaccount=(TextView)convertView.findViewById(R.id.dev_listact_setaccount);
+            holder.setusername=(TextView)convertView.findViewById(R.id.dev_listact_setusername);
+            holder.setphone=(TextView)convertView.findViewById(R.id.dev_listact_setphone);
+            holder.setpassword=(TextView)convertView.findViewById(R.id.dev_listact_setpassword);
+            convertView.setTag(holder);
+        }else{
+            holder=(listview_dev_account.ViewHolder)convertView.getTag();
+        }
+        holder.setaccount.setText((String)list.get(position).get("setaccount"));
+        holder.setusername.setText((String)list.get(position).get("setusername"));
+        holder.setphone.setText((String)list.get(position).get("setphone"));
+        holder.setpassword.setText((String)list.get(position).get("setpassword"));
+        return convertView;
     }
 }
--- app/src/main/java/com/example/oop_travel_app/devlop_function/listview_dev_order.java
+++ app/src/main/java/com/example/oop_travel_app/devlop_function/listview_dev_order.java
@@ -1,11 +1,14 @@
 package com.example.oop_travel_app.devlop_function;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import com.example.oop_travel_app.R;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -23,26 +26,43 @@ public class listview_dev_order extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return list.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        Object obj=list.get(position);
+        return list.indexOf(obj);
     }
     private static class ViewHolder{
-        TextView triptitle;
-        TextView priceinterval;
-        TextView dateinterval;
+        TextView orderid;
+        TextView userid;
+        TextView tripid;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public View getView(int position, View convertView, ViewGroup parent){
+        listview_dev_order.ViewHolder holder;
+        if(convertView==null){
+        convertView= LayoutInflater.from(context).inflate(R.layout.listview_dev_order, parent,false);
+        holder =new listview_dev_order.ViewHolder();
+
+        holder.orderid=(TextView)convertView.findViewById(R.id.dev_listorder_setorderid);
+        holder.userid=(TextView)convertView.findViewById(R.id.dev_listorder_setusername);
+        holder.tripid=(TextView)convertView.findViewById(R.id.dev_listorder_settripid);
+        convertView.setTag(holder);
+    }else{
+        holder=(listview_dev_order.ViewHolder)convertView.getTag();
+    }
+        holder.orderid.setText(String.valueOf(list.get(position).get("orderid")));
+        holder.userid.setText(String.valueOf(list.get(position).get("userid")));
+        holder.tripid.setText(String.valueOf(list.get(position).get("tripid")));
+
+        return convertView;
     }
 }
