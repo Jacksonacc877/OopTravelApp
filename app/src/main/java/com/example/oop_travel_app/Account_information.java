0d8ed1af6d52e1966b1c96195d8e73842a9760cf
jacksonacc877 <b07505035@ntu.edu.tw>
2020-06-22 11:12:26 +0800

    qqqqqq

--- app/src/main/java/com/example/oop_travel_app/Account_information.java
+++ app/src/main/java/com/example/oop_travel_app/Account_information.java
@@ -26,8 +26,7 @@ public class Account_information extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_information);
         Bundle bundle=this.getIntent().getExtras();
-//        str_phones=bundle.getString("str_phones");
-        str_phones=Check_login.pphone;
+        str_phones=bundle.getString("str_phones");
         str_name=bundle.getString("str_name");
         str_userID=bundle.getString("str_userID");
         str_passwordss=bundle.getString("str_passwordss");
